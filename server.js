b9c406e3b87883e0c84517ce8ae38bdc277ae08d
Tanisha Gupta <tanishagupta382@gmail.com>
2023-07-02 16:34:23 +0530

    fourth commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const serverConfig=require('./configs/server.config');
 const mongoose=require("mongoose");
 const dbConfig=require('./configs/db.config');
 const app=express()
+const userModel = require('./models/user.model');
 
 /**
  * Logic to connect to MongoDB and create an Admin user
@@ -15,7 +16,33 @@ db.on("error",()=>{
 });
 db.once("open",()=>{
     console.log("DB is connected");
+
+    init();
 })
+
+async function  init(){
+    /*
+    *check if the admin user is already present
+    
+    */
+   let admin=await userModel.findOne({
+    userId:"admin"
+   })
+   if(admin){
+    console.log("Admin is already present");
+    return;
+   }
+
+   admin=await userModel.create({
+    name:"Tanisha Gupta",
+    userId:"admin",
+    email:"tanishagupta382@gmail.com",
+    userType:"ADMIN",
+    password:"Welcome1"
+   });
+   console.log(admin);
+   
+}
 
 app.listen(serverConfig.PORT,()=>{
     console.log(`Server Started on the port number ${serverConfig.PORT}`);
